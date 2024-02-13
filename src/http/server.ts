821b4b3006f92997e562849eb8e6732fe4277bd6
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-02-13 14:33:11 -0300

    chore: Add Zod

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,9 @@
 import fastify from 'fastify'
+import { createPoll } from './routes/create-poll'
 
 const app = fastify()
+
+app.register(createPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
